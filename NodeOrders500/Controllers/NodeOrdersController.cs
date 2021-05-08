116ceba72ad6c0aa0779d47c3925c376274afe30
Amber Dulz <amdulz@gmail.com>
2021-05-08 14:28:42 -0700

    new

---
+++ NodeOrders500/Controllers/NodeOrdersController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace NodeOrders500.Controllers
+{
+    
+    public class NodeOrdersController : ApiController
+    {
+        NodeOrders500Entities myNodeOrdersDB = new NodeOrders500Entities();
+        public IEnumerable<Order> GetAllorders()
+        {
+            return myNodeOrdersDB.Orders;
+        }
+    }
+}
---
+++ NodeOrders500/Scripts/App1.js
@@ -0,0 +1,39 @@
+﻿
+var uri = 'api/NodeOrders';
+
+
+
+
+
+function find() {
+    $('#saveResponse').text = '';
+    $("#notes").empty();
+    var id = $('#SearchId').val();
+    $.getJSON(uri + '/' + id)
+        .done(function (data) {
+            $('#note').text(formatItem(data));
+        })
+        .fail(function (jqXHR, textStatus, err) {
+            $('#note').text('Error: ' + err);
+        });
+}
+
+
+
+
+
+function findMarkup() {
+    $("#markups").empty();
+    var id = $('#GetMarkups').val();
+    $.getJSON("api/Query1" + '/' + id)
+        .done(function (data) {
+            // On success, 'data' contains a list of products.
+            $.each(data, function (key, item) {
+                // Add a list item for the product.
+                $('<li>', { text: formatItem(item) }).appendTo($('#markups'));
+            });
+        });
+}
+function formatItem(item) {
+    return item.storeID + ':   ' + item.pricePaid;
+}
